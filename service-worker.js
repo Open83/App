a9325529e316197cefee23dfbb3857d6713db67e
Open83 <125194839+Open83@users.noreply.github.com>
2025-09-23 12:21:38 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,14 @@
 const cacheName = "habit-tracker-v1";
 const filesToCache = [
-  "/",
-  "/index.html",
-  "/gallery.html",
-  "/style.css",
-  "/app.js",
-  "/gallery.js",
+  "/", "/index.html", "/gallery.html",
+  "/style.css", "/app.js", "/gallery.js",
   "/manifest.json"
 ];
 
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open(cacheName).then((cache) => cache.addAll(filesToCache))
-  );
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(filesToCache)));
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
-  );
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
