b7cc05bc1e7a25bb8078b480d746c1aa43800260
Open83 <125194839+Open83@users.noreply.github.com>
2025-09-23 11:49:14 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,22 @@
-const CACHE_NAME = "habit-tracker-v1";
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./gallery.html",
-  "./style.css",
-  "./app.js",
-  "./gallery.js",
-  "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+const cacheName = "habit-tracker-v1";
+const filesToCache = [
+  "/",
+  "/index.html",
+  "/gallery.html",
+  "/style.css",
+  "/app.js",
+  "/gallery.js",
+  "/manifest.json"
 ];
 
-self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => cache.addAll(filesToCache))
+  );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
   );
 });
